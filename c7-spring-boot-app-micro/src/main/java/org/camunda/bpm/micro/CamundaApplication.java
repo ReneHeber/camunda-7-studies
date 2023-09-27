8d59c37f109fb5f91258cc32efe26d4cc33da5ed
rhe <rhe@novatec-gmbh.de>
2023-09-27 09:39:31 +0200

    New module for c7 micro course

---
+++ c7-spring-boot-app-micro/src/main/java/org/camunda/bpm/micro/CamundaApplication.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.micro;
+
+/**
+ * Hello world!
+ *
+ */
+public class CamundaApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ c7-spring-boot-app-micro/src/test/java/org/camunda/bpm/micro/CamundaApplicationTest.java
@@ -0,0 +1,38 @@
+package org.camunda.bpm.micro;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple CamundaApplication.
+ */
+public class CamundaApplicationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CamundaApplicationTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CamundaApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
